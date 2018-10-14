1ee7f80c3513ed42125ef14ae328633132761c4c
gckeny <43816933+gckeny@users.noreply.github.com>
2018-10-14 23:57:58 +0800

    网上找了个网页自动登录代码-zddl.py study.md加了如何自动打开浏览器的说明

--- code/zddl.py
+++ code/zddl.py
@@ -4,9 +4,13 @@ from PIL import Image, ImageEnhance
 import pytesseract
 import os,time
 import PIL.ImageOps
+
 chromedriver = "D:\chromedriver.exe"  # 这里写本地的chromedriver 的所在路径
 os.environ["webdriver.Chrome.driver"] = chromedriver  # 调用chrome浏览器
-driver = webdriver.Chrome(chromedriver)
+# 加启动配置,启动浏览器不再出现"Chrome正在受到自动软件的控制""
+option = webdriver.ChromeOptions()
+option.add_argument('disable-infobars')
+driver = webdriver.Chrome(chromedriver, chrome_options=option)
 #driver.get("http://221.192.190.15:7001/")  # 该处为具体网址
 driver.get("http://121.28.169.98:8080/admin/")  # 该处为具体网址
 driver.refresh() #刷新页面
@@ -24,14 +28,7 @@ a = Image.open("a.png")
 im = a.crop((left,top,right,bottom))
 im.save("b.png")
 time.sleep(1)
-def initTable(threshold=230):
-    table = []
-    for i in range(256):
-        if i < threshold:
-            table.append(0)
-        else:
-            table.append(1)
-    return table
+
 # 打开保存的验证码图片
 image = Image.open("b.png")
 # 图片转换成字符
@@ -55,27 +52,22 @@ def initTable(threshold=140):
     return table
 
 
-#图片的处理过程
+# 图片的处理过程
 im = image.convert('L')
 binaryImage = im.point(initTable(), '1')
 im1 = binaryImage.convert('L')
 im2 = PIL.ImageOps.invert(im1)
 im3 = im2.convert('1')
 im4 = im3.convert('L')
-#将图片中字符裁剪保留
-# box = (30,10,90,28)
-# region = im4.crop(box)
-#将图片字符放大
-out = im4.resize((120,38))
+
+# 将图片字符放大
+out = im1.resize((120,38))
 time.sleep(1)
 vcode = pytesseract.image_to_string(out)
-time.sleep(1)
-asd = pytesseract.image_to_string(im4)
-
-
-im4.save("b.png")
 out.save('3.png')
-print("lll:",vcode,'im4',asd)
+print("lll:", vcode)
+
+
 # 填充用户名 密码 验证码
 #driver.find_element_by_name("login").send_keys("gccl_liucuifen")
 driver.find_element_by_name("userName").send_keys("sjz")
@@ -86,4 +78,4 @@ driver.find_element_by_name("code").send_keys(vcode)
 #elem_pwd = driver.find_element_by_name("password")
 #elem_pwd.send_keys(Keys.RETURN)
 
-#driver.find_element_by_class_name("button").click()
+driver.find_element_by_class_name("button").click()
--- study.md
+++ study.md
@@ -56,3 +56,43 @@
 图片title是图片的标题，当鼠标移到图片上时显示的内容。title可加可不加
 ![blockchain](https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/
 u=702257389,1274025419&fm=27&gp=0.jpg "区块链")
+
+
+三. 安装三大浏览器驱动driver
+
+???? 1.chromedriver 下载地址：https://code.google.com/p/chromedriver/downloads/list
+
+???? 2.Firefox的驱动geckodriver 下载地址：https://github.com/mozilla/geckodriver/releases/
+
+???? 3.IE的驱动IEdriver 下载地址：http://www.nuget.org/packages/Selenium.WebDriver.IEDriver/
+
+
+
+注意：下载解压后，将chromedriver.exe , geckodriver.exe , Iedriver.exe发到Python的安装目录，例如 D:\python 。 然后再将Python的安装目录添加到系统环境变量的Path下面。
+
+然后打开Python IDLE分别输入以下代码来启动不同的浏览器
+
+
+
+启动谷歌浏览器
+
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('http://www.baidu.com/')
+
+
+
+启动火狐浏览器
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://www.baidu.com/')
+
+
+启动IE浏览器
+from selenium import webdriver
+
+browser = webdriver.Ie()
+browser.get('http://www.baidu.com/')
+--------------------- 
